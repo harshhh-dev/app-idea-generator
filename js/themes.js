809ab60b7ec0f365f0d8b69729dd04f2136628ce
Harsh Singh <harshhh.singh.personal@gmail.com>
2020-10-05 14:34:24 -0400

    Added lightmode

--- js/themes.js
+++ js/themes.js
@@ -1,7 +1,19 @@
-var mode = document.getElementById('themeMode');
+var darkMode = false
 
-if (mode.checked == true) {
-    console.log("True!")
-} else {
-    console.log('False!')
+function ChangeColourScheme() {
+    if (darkMode) {
+        document.getElementById('body').className = 'light'
+
+        document.getElementById('generateButton').className = 'light-button'
+        document.getElementById('idea').className = 'light-text'
+
+        darkMode = false
+    } else {
+        document.getElementById('body').className = 'dark'
+
+        document.getElementById('generateButton').className = 'dark-button'
+        document.getElementById('idea').className = 'dark-text'
+
+        darkMode = true
+    }
 }
