88cac4612ea4f87fd429f95086f66fc4e988e32b
Harsh Singh <harshhh.singh.personal@gmail.com>
2020-10-05 15:16:39 -0400

    Styled dark mode buttons

--- js/themes.js
+++ js/themes.js
@@ -7,12 +7,16 @@ function ChangeColourScheme() {
         document.getElementById('generateButton').className = 'light-button'
         document.getElementById('idea').className = 'light-text'
 
+        document.getElementById('toggle').innerHTML = 'Turn Off The Lights'
+
         darkMode = false
     } else {
         document.getElementById('body').className = 'dark'
 
         document.getElementById('generateButton').className = 'dark-button'
         document.getElementById('idea').className = 'dark-text'
+
+        document.getElementById('toggle').innerHTML = 'Turn On The Lights'
 
         darkMode = true
     }
