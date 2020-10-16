d294083ba1f9db0cf9415eb252a953dae69464fa
itsvvishnu <vishnushanmukan@gmail.com>
2020-10-16 22:50:49 +0530

    toggle button style change

--- js/themes.js
+++ js/themes.js
@@ -29,16 +29,21 @@ changeColourScheme = () => {
         document.getElementById('generateButton').className = 'light-button'
         document.getElementById('idea').className = 'light-text'
 
-        document.getElementById('toggle').innerHTML = 'Turn Off The Lights'
+        document.getElementById('toggle').innerHTML = 'Lights Turned On'
+
+        document.querySelector(".toggle-wrapper").classList.add("active")
+        document.getElementById('toggle').classList.add("active")
         localStorage.setItem("theme", "light");
         darkMode = false
     } else {
         document.getElementById('body').className = 'dark'
+        document.getElementById('toggle').classList.remove("active")
+        document.querySelector(".toggle-wrapper").classList.remove("active")
 
         document.getElementById('generateButton').className = 'dark-button'
         document.getElementById('idea').className = 'dark-text'
+        document.getElementById('toggle').innerHTML = 'Lights Turned Off'
 
-        document.getElementById('toggle').innerHTML = 'Turn On The Lights'
         localStorage.setItem("theme", "dark");
         darkMode = true
     }
